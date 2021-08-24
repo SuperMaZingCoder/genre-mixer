8835d9ac67c8e2e1940e2d782f40c7a2aa3543ff
SuperMaZingCoder <supermazingcoder@gmail.com>
2021-08-24 16:59:10 -0400

    Created the minimum viable project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Button.css";
+import "./Card.css";
+import genres from "./genres";
+import shuffle from "./shuffle";
 
 function App() {
+  const [mixedGenres, setMixedGenres] = useState(null);
+
+  function mixGenres() {
+    shuffle(genres);
+    setMixedGenres(genres.slice(0, 2));
+  }
+
   return (
-    <div>
-      <p>Genre Mixer</p>
+    <div className="h-full flex flex-col justify-center items-center">
+      <div className="flex-grow-0 flex-shrink Card flex flex-col gap-10">
+        <button className="Button" onClick={mixGenres}>
+          MIX GENRES
+        </button>
+        {mixedGenres ? (
+          <p className="text-center">{mixedGenres.join(" + ")}</p>
+        ) : null}
+      </div>
     </div>
   );
 }
